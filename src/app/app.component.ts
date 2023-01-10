9a98bc84894ee8da226d767c24387db9edc72a8e
samscreen9 <58282761+samscreen9@users.noreply.github.com>
2023-01-10 15:27:37 +0530

    debounce,debounceTime,throttle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,7 +22,7 @@ import {
   forkJoin,
   merge,
   skip,
-  skipUntil,skipWhile, distinct, from, distinctUntilChanged, distinctUntilKeyChanged, filter, first, elementAt
+  skipUntil,skipWhile, distinct, from, distinctUntilChanged, distinctUntilKeyChanged, filter, first, elementAt,debounceTime, pipe, debounce,throttleTime
 } from 'rxjs';
 
 @Component({
@@ -34,29 +34,47 @@ export class AppComponent implements OnInit {
 name = 'Angular';
 /*returns elements emitted by the source that satisfies condition*/
 ngOnInit(){
-of(1,2,3,4).pipe(filter((x)=>x%2==0)).subscribe(
+let inp1=document.getElementById("input1");
+/*
+let x1=fromEvent(inp1,'input')
+.pipe(
+  map((i:any) => i.target.value),
+  debounceTime(500),
+ // distinctUntilChanged()
+).subscribe(
   (data)=>{console.log(data);}
 )
+*/
 
-fromEvent(document,'click')
-.pipe(filter((e:Event)=>{
-  return (e.target as HTMLElement).tagName=='P'
-})).subscribe(
+/*
+let intTime=200;
+let x2=fromEvent(inp1,'input')
+.pipe(
+  map((i:any) => i.target.value),
+  debounce(x =>{//intTime+=1000;
+               // console.log(intTime);
+                return interval(intTime)}
+                ),
+  distinctUntilChanged()
+).subscribe(
   (data)=>{console.log(data);}
 )
+*/
 
-/*first*/
-of(1,2,3,4).pipe(first((x)=>x%2==0)).subscribe(
+/*****debounce debounceTime and throttle (run individually by commenting other) 
+   are used to improve the performance of the application e.g consider you are working on search functionality
+  a default behaviour will be on every entered text a new request will be sent, this will used resource from both client and server, the server will recieve unnecessary extra load of requests. debouceTime(1000) will wait till the time difference between you intering inputs or clicks is more than 1000, same for debounce but debounce instead of taking time directly is dependent on other observable that gives out time period.
+ throttle on other hand will always create new request after certain delay if you are putting inputs 
+*/
+
+fromEvent(inp1,'input')
+.pipe(map((x:any)=>x.target.value),
+throttleTime(1000)
+//distinctUntilChanged()
+).subscribe(
   (data)=>{console.log(data);}
 )
-console.log("..................ElementAT.....")
-/*ElementAt*/
-of(1,2,3,4).pipe(elementAt(2,8)).subscribe(
-  (data)=>{console.log(data);} /*try get find value at 2 if not send value 8*/
-)
-
 
 }
-
 
 }
