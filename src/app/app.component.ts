9e2fc100050d20da5d967172897b547a1cae00cc
samscreen9 <58282761+samscreen9@users.noreply.github.com>
2023-01-10 15:31:56 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,7 +22,7 @@ import {
   forkJoin,
   merge,
   skip,
-  skipUntil,skipWhile, distinct, from, distinctUntilChanged, distinctUntilKeyChanged, filter, first, elementAt,debounceTime, pipe, debounce,throttleTime
+  skipUntil,skipWhile, distinct, from, distinctUntilChanged, distinctUntilKeyChanged, filter, first, elementAt
 } from 'rxjs';
 
 @Component({
@@ -34,99 +34,29 @@ export class AppComponent implements OnInit {
 name = 'Angular';
 /*returns elements emitted by the source that satisfies condition*/
 ngOnInit(){
-let inp1=document.getElementById("input1");
-/*
-let x1=fromEvent(inp1,'input')
-.pipe(
-  map((i:any) => i.target.value),
-  debounceTime(500),
- // distinctUntilChanged()
-).subscribe(
-  (data)=>{console.log(data);}
-)
-*/
-
-/*
-let intTime=200;
-let x2=fromEvent(inp1,'input')
-.pipe(
-  map((i:any) => i.target.value),
-  debounce(x =>{//intTime+=1000;
-               // console.log(intTime);
-                return interval(intTime)}
-                ),
-  distinctUntilChanged()
-).subscribe(
-  (data)=>{console.log(data);}
-)
-*/
-
-/*****debounce debounceTime and throttle (run individually by commenting other) 
-   are used to improve the performance of the application e.g consider you are working on search functionality
-  a default behaviour will be on every entered text a new request will be sent, this will used resource from both client and server, the server will recieve unnecessary extra load of requests. debouceTime(1000) will wait till the time difference between you intering inputs or clicks is more than 1000, same for debounce but debounce instead of taking time directly is dependent on other observable that gives out time period.
- throttle on other hand will always create new request after certain delay if you are putting inputs 
-*/
-
-fromEvent(inp1,'input')
-.pipe(map((x:any)=>x.target.value),
-throttleTime(1000)
-//distinctUntilChanged()
-).subscribe(
+of(1,2,3,4).pipe(filter((x)=>x%2==0)).subscribe(
   (data)=>{console.log(data);}
 )
 
-}
+fromEvent(document,'click')
+.pipe(filter((e:Event)=>{
+  return (e.target as HTMLElement).tagName=='P'
+})).subscribe(
+  (data)=>{console.log(data);}
+)
+
+/*first*/
+of(1,2,3,4).pipe(first((x)=>x%2==0)).subscribe(
+  (data)=>{console.log(data);}
+)
+console.log("..................ElementAT.....")
+/*ElementAt*/
+of(1,2,3,4).pipe(elementAt(2,8)).subscribe(
+  (data)=>{console.log(data);} /*try get find value at 2 if not send value 8*/
+)
+
 
 }
 
-/*  js creating debounce and throttle
 
-</head>
-<body>
-<input type="text" id="input1"><br>
-Default:<span id="p1"></span><br>
-Debounce:<span id="p2"></span><br>
-Throttle:<span id="p3"></span><br>
-</body>
-</html>
-
-
-let inp1= document.getElementById("input1");
-let df=document.getElementById("p1");
-let de=document.getElementById("p2");
-let th=document.getElementById("p3");
-
-inp1.addEventListener("input",(e)=>{
-df.textContent=e.target.value;
-betterThanExpensive(e.target.value);
-betterThanExpensiveThrottle(e.target.value);
-})
-
-function expensive(text){
-return  de.textContent=text;
 }
-function expensiveth(text){
-return  th.textContent=text;
-}
-
-let betterThanExpensive=debounce(expensive);
-let betterThanExpensiveThrottle=throttle(expensiveth);
-
-function debounce(cb){
-let tm;
-return (...args)=>{
-clearTimeout(tm);
-tm=setTimeout(()=>{cb(...args)},1000);
-}
-}
-
-function throttle(cb){
-let f=true;
-return (...args)=>{
-if(f)
- cb(...args);
- f=false
- setTimeout(()=>{f=true},1000)
-}
-}
-*/
